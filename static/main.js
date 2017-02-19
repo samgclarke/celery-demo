572a04221f0d101f1a729da52b926af6f4f3c755
Sam Clarke <samclarke.g@gmail.com>
2017-02-19 16:20:44 -0400

    improve console messages and add docstrings

--- static/main.js
+++ static/main.js
@@ -1,16 +1,20 @@
 $( document ).ready(function() {
-    console.log("task_id:", task_id);
-
+    console.log("Our celery task_id:", task_id);
+    
+    //  Here we poll the Flask API for the status of the Celery task.
     (function poll() {
 	    setTimeout(function() {
 		    $.get("/status/" + task_id, function(result, status){
 		    	var data = jQuery.parseJSON(result);
 	        console.log('data:', data.state);
+	        //  Here we keep polling until the task has completed successfully. 
+          //  Note that in a production application we would want to handle 
+          //  FAILURE and REVOKED states also!
 	        if (data.state === 'PENDING') {
             poll();
 	        } else {
-	        	console.log('That\'s it from me!');
 	        	if (data.state === 'SUCCESS') {
+	        		console.log('Task completed successfully. Phew! I can stop polling now :)');
 	        		$('#result').html(data.result.toString());
 	        	} 
 	        }
